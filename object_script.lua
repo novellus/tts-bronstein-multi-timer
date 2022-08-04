1618e99f0bf2328fdb6543914e7299ce6664b04d
novellus <novellus.cato@gmail.com>
2022-08-04 14:23:29 -0700

    bug fixes

--- object_script.lua
+++ object_script.lua
@@ -100,7 +100,7 @@ function generate_ui()
 
     self.createButton(concat_tables(
         button_template, {
-            click_function = 'start_timer',
+            click_function = 'start_this_timer',  -- does not execute start_timer directly, since start_this_timer does init validation
             label          = 'Start this timer',
             position       = {-0.37, v_pos, -11/34 + 10/34*6/23},
             color          = 'Green',
@@ -163,7 +163,7 @@ function generate_ui()
             font_size      = 200,
             color          = 'White',
             position       = {0.37, v_pos, -11/34 + 10/34*6/23},
-            tooltip        = 'Any number. Determines which timer is triggered when "Next" button is used, in ascending round-robin order.',
+            tooltip        = 'Any positive number. Determines which timer is triggered when "Next" button is used, in ascending round-robin order.',
             alignment      = 3, -- center aligned
             -- validation     = 3, -- float
         })
@@ -215,13 +215,14 @@ function test_button()
         printToAll('    ' .. tostring(key) .. ': ' .. tostring(val), 'White')
     end
 
-    -- -- family
-    -- printToAll('next_relative: ' .. tostring(next_relative.getVar('self').guid), 'White')
-    -- printToAll('family:', 'White')
-    -- family = Global.getTable(GLOBAL_COORDINATION_VAR)
-    -- for relative, _ in pairs(family) do
-    --     printToAll('    ' .. tostring(relative) .. ': ' .. tostring(relative.getVar('self').guid), 'White')
-    -- end
+    -- family
+    printToAll('next_relative: ' .. tostring(next_relative.getVar('self').guid), 'White')
+    printToAll('earliest_relative: ' .. tostring(earliest_relative.getVar('self').guid), 'White')
+    printToAll('family:', 'White')
+    family = Global.getTable(GLOBAL_COORDINATION_VAR)
+    for relative, _ in pairs(family) do
+        printToAll('    ' .. tostring(relative) .. ': ' .. tostring(relative.getVar('self').guid) .. ', ' .. tostring(relative.getVar('state').turn), 'White')
+    end
 
     -- -- inputs list
     -- printToAll('inputs:', 'White')
@@ -284,7 +285,7 @@ function validate_input_edit(input_value, still_editing)
             return 1
         else
             if not validate_float_string(input_value) then
-                printToAll('Input value is invalid, must be a valid number (int or float): "' .. input_value .. '"', 'White')
+                printToAll('Manual input value is invalid, must be a number (positive int or float): "' .. input_value .. '"', 'White')
                 return 1
             else
                 return 2
@@ -307,16 +308,12 @@ function start_timer()
     -- starts this timer
     -- resets bronstein field, logs start time, and schedules an update loop
 
-    if state.any_timer_running then
-        printToAll('A timer is already running, cannot start this one! Use "Reset" and then "Start" if you want to restart.', 'White')
-        return
-    end
+    -- TODO
+    -- validate_turn_order()
 
     -- update state
     state.timer_running = true
-    for relative, _ in pairs(family) do
-        relative.setVar('any_timer_running', true)
-    end
+    instruct_any_timer_running(true)
 
     -- reset bronstein field on start
     state.bronstein_time_remaining = state.bronstein_time_original
@@ -333,14 +330,58 @@ end
 
 function instruct_start_earliest_timer()
     -- instructs earliest relative (possibly self) to start
-    earliest_relative.call('start_timer')
+    if validate_user_call_to_start() then
+        earliest_relative.call('start_timer')
+    end
+end
+
+
+function start_this_timer()
+    -- executes from like named button
+    -- runs validation before executing start_timer
+    if validate_user_call_to_start() then
+        start_timer()
+    end
+end
+
+
+function validate_user_call_to_start()
+    -- validates user start call is ok to execute
+    
+    -- no timer running
+    if state.any_timer_running then
+        printToAll('A timer is already running, cannot start this one! Use "Reset" and then "Start" if you want to restart.', 'White')
+        return false
+    end
+
+    -- validate turn order fields are unique
+    -- Cannot validate this exclusively at edit time, since object cloning causes duplicate entries
+    -- Easier just to validate it here than cover many cases
+    turns = {}
+    for relative, _ in pairs(family) do
+        relative_turn = relative.getVar('state').turn
+        if turns[relative_turn] ~= nil then
+            printToAll('Turn order fields are not unique, refusing to start timers! Make all turn fields unique, and then try again.', 'White')
+            return false
+        else
+            turns[relative_turn] = true
+        end
+    end
+
+    return true
+end
+
+
+function set_any_timer_running(value)
+    -- encapsulated function since it sets a state variable
+    state.any_timer_running = value
 end
 
 
 function instruct_any_timer_running(value)
-    -- instructs relatives that one of the timers has started, which locks out UI editing
+    -- instructs relatives to set/clear any_timer_running
     for relative, _ in pairs(family) do
-        relative.setVar('any_timer_running', value)
+        relative.call('set_any_timer_running', value)
     end
 end
 
@@ -365,8 +406,8 @@ function instruct_reset_timers()
     -- instructs entire family to reset
     for relative, _ in pairs(family) do
         relative.call('reset_timer')
-        relative.setVar('any_timer_running', false)
     end
+    instruct_any_timer_running(false)
 end
 
 
@@ -388,8 +429,8 @@ function instruct_pause_timers()
     -- instructs entire family to pause
     for relative, _ in pairs(family) do
         relative.call('pause_timer')
-        relative.setVar('any_timer_running', false)
     end
+    instruct_any_timer_running(false)
 end
 
 
@@ -429,7 +470,7 @@ function update_timer()
         end
 
         -- update UI
-        self.editInput({index=pool_time_edit_index, value=format_time(state.pool_time_remaining, true)})
+        self.editInput({index=pool_time_edit_index, value=format_time(state.pool_time_remaining, false)})
 
         -- trigger next turn if pool time ran out
         if state.pool_time_remaining <= 0 then
@@ -442,7 +483,6 @@ end
 
 function next_turn()
     -- Passes turn to the next player, pausing this timer, and starting the next timer
-
     if not state.timer_running then
         printToAll('This timer is not running, ignoring attempt to go to next turn.', 'White')
     else
@@ -458,27 +498,32 @@ function update_family()
     family = Global.getTable(GLOBAL_COORDINATION_VAR)
 
     -- compute next relative in turn order
-    earliest_turn = nil
+    earliest_relative_turn = nil
     earliest_relative = nil
-    next_turn = nil
+    next_relative_turn = nil
     next_relative = nil
 
     for relative, _ in pairs(family) do
         relative_turn = relative.getVar('state').turn
 
-        if earliest_turn == nil or relative_turn < earliest_turn then
-            earliest_turn = relative_turn
-            earliest_relative = relative_relative
+        if earliest_relative_turn == nil or relative_turn < earliest_relative_turn then
+            earliest_relative_turn = relative_turn
+            earliest_relative = relative
         end
 
-        if state.turn < relative_turn and relative_turn < next_turn then
-            next_turn = relative_turn
-            next_relative = relative_relative
+        if state.turn < relative_turn and (next_relative_turn == nil or relative_turn < next_relative_turn) then
+            next_relative_turn = relative_turn
+            next_relative = relative
         end
     end
 
     -- next in turn order if there is anyone, or wrap around to earliest
-    next_relative = next_relative or earliest_relative or self
+    next_relative = next_relative or earliest_relative
+
+    -- warn on family update while running, which is user error
+    if state.running then
+        printToAll('Warning: timer family was updated while a timer was running, may get glitchy?', 'White')
+    end
 end
 
 
@@ -556,6 +601,9 @@ function onLoad(saved_state)
             end
         end
     end
+
+    -- wait for self to finish spawning in the game engine
+    -- while self.spawning do end
 
     -- communicate with other timers
     add_self_to_family()
