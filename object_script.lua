14d04b4733e5b563f4a13c92780ead7cee4eab37
novellus <novellus.cato@gmail.com>
2022-08-03 17:47:57 -0700

    implementing timers

--- object_script.lua
+++ object_script.lua
@@ -1,6 +1,11 @@
 -- used to corrdinate between all timer clones
 GLOBAL_COORDINATION_VAR = 'tts_bronstein_multi_timer'
 
+-- Update frequency for the main timer loop, which should only run on one timer at a time.
+-- Defines update frequency for the UI and alarm functionality
+-- As well as the tolerance on the bronstein-to-pool time transition
+UPDATE_PERIOD = 0.1  -- in seconds
+
 
 function concat_tables(table1, table2)
     -- util function
@@ -34,7 +39,12 @@ function format_time(seconds, plus)
         sign = ''
     end
 
-    return string.format('%s%02d:%02d:%02d', sign, hours, minutes, seconds)
+    return string.format('%s%02d:%02d:%05.2f', sign, hours, minutes, seconds)
+end
+
+
+function gui_nop()
+    -- endpoint function for a button without click functionality
 end
 
 
@@ -79,7 +89,7 @@ function generate_ui()
 
     self.createButton(concat_tables(
         button_template, {
-            click_function = 'test_button',
+            click_function = 'instruct_start_earliest_timer',
             label          = 'Start first timer',
             position       = {-0.37, v_pos, -11/34 - 10/34*6/23},
             color          = 'Green',
@@ -90,7 +100,7 @@ function generate_ui()
 
     self.createButton(concat_tables(
         button_template, {
-            click_function = 'test_button',
+            click_function = 'start_timer',
             label          = 'Start this timer',
             position       = {-0.37, v_pos, -11/34 + 10/34*6/23},
             color          = 'Green',
@@ -101,7 +111,7 @@ function generate_ui()
 
     self.createButton(concat_tables(
         button_template, {
-            click_function = 'test_button',
+            click_function = 'instruct_reset_timers',
             label          = 'Reset All',
             position       = {-0.11, v_pos, -11/34},
             color          = 'Red',
@@ -111,7 +121,7 @@ function generate_ui()
 
     self.createButton(concat_tables(
         button_template, {
-            click_function = 'test_button',
+            click_function = 'instruct_pause_timers',
             label          = 'Pause All',
             position       = {0.13, v_pos, -11/34},
             tooltip        = 'Pauses all timers',
@@ -120,7 +130,7 @@ function generate_ui()
 
     self.createButton(concat_tables(
         button_template, {
-            click_function = 'test_button',
+            click_function = 'next_turn',
             label          = 'Next\nTurn',
             font_color     = 'Black',
             font_size      = 250,
@@ -135,7 +145,7 @@ function generate_ui()
     -- turn order field
     self.createButton(concat_tables(
         button_template, {
-            click_function = 'test_button',
+            click_function = 'gui_nop',
             label          = 'Turn Order',
             font_color     = 'Black',
             height         = 0,  -- text display, not an actual button. There is no pure text tool.
@@ -148,6 +158,7 @@ function generate_ui()
         button_template, {
             input_function = 'turn_order_edited',
             label          = '1',
+            value          = tostring(state.turn),
             font_color     = 'Black',
             font_size      = 200,
             color          = 'White',
@@ -168,14 +179,15 @@ function generate_ui()
         font_color     = 'Black',
         scale          = UI_scale,
         label          = '+00:00:00.00',
-        alignment      = 2, -- left aligned
-        validation     = 3, -- float
+        alignment      = 4, -- right aligned
+        -- validation  = built-in validation cannot be used, since we want to display ':' and '+' symbols
     }
 
     self.createInput(concat_tables(
         input_template, {
             input_function = 'pool_time_edited',
             position       = {-0.13, v_pos, 0},
+            value          = format_time(state.pool_time_remaining, false),
             tooltip        = 'Pool time remaining, used after Bronstein Time runs out each turn.',
         })
     )
@@ -185,6 +197,7 @@ function generate_ui()
         input_template, {
             input_function = 'bronstein_time_edited',
             position       = {-0.13, v_pos, 11/34},
+            value          = format_time(state.bronstein_time_remaining, true),
             tooltip        = 'Bronstein Time remaining. Refreshed each turn, and used up before pool time is used.',
         })
     )
@@ -236,15 +249,11 @@ end
 function pool_time_edited(obj, player_clicker_color, input_value, still_editing)
     validity = validate_input_edit(input_value, still_editing)
     if validity == 1 then
-
-        printToAll('testedit: ' .. tostring(state) .. ': ' .. tostring(state.pool_time_remaining), 'White')
-        for key, val in pairs(state) do
-            printToAll('    ' .. tostring(key) .. ': ' .. tostring(val), 'White')
-        end
-
         return format_time(state.pool_time_remaining, false)  -- return value from this callback sets input state
     elseif validity == 2 then
         state.pool_time_original = tonumber(input_value)
+        state.pool_time_remaining = state.pool_time_original
+        return format_time(state.pool_time_original, false)  -- return value from this callback sets input state
     end
 end
 
@@ -255,6 +264,8 @@ function bronstein_time_edited(obj, player_clicker_color, input_value, still_edi
         return format_time(state.bronstein_time_remaining, true)  -- return value from this callback sets input state
     elseif validity == 2 then
         state.bronstein_time_original = tonumber(input_value)
+        state.bronstein_time_remaining = state.bronstein_time_original
+        return format_time(state.bronstein_time_original, true)  -- return value from this callback sets input state
     end
 end
 
@@ -269,11 +280,11 @@ function validate_input_edit(input_value, still_editing)
         return 0
     else
         if state.any_timer_running then
-            printToAll('Cannot update parameters while timer is running!', 'White')
+            printToAll('Cannot update parameters while any timer is running! Use "Pause All" or "Reset All" if you want to edit fields.', 'White')
             return 1
         else
             if not validate_float_string(input_value) then
-                printToAll('Input value is invalid, must be a valid number (int,float): ' .. input_value, 'White')
+                printToAll('Input value is invalid, must be a valid number (int or float): "' .. input_value .. '"', 'White')
                 return 1
             else
                 return 2
@@ -286,21 +297,159 @@ end
 function validate_float_string(s)
     -- validates string before numerical conversion
     -- atleast one side of the decimal must contain a number, so can't do double star
-    return (string.find(s, '^%d+(%.%d*)?$|^%.%d+$') ~= nil)
+    valid =          string.find(s, '^%d+%.?%d*$') ~= nil
+    valid = valid or string.find(s,    '^%.%d+$')  ~= nil
+    return valid
 end
 
 
 function start_timer()
---TODO
+    -- starts this timer
+    -- resets bronstein field, logs start time, and schedules an update loop
+
+    if state.any_timer_running then
+        printToAll('A timer is already running, cannot start this one! Use "Reset" and then "Start" if you want to restart.', 'White')
+        return
+    end
+
+    -- update state
+    state.timer_running = true
+    for relative, _ in pairs(family) do
+        relative.setVar('any_timer_running', true)
+    end
+
+    -- reset bronstein field on start
+    state.bronstein_time_remaining = state.bronstein_time_original
+    state.counting_bronstein = true
+
+    -- record timer start clock-time
+    countdown_start_time = os.clock()
+    pool_time_remaining_at_countdown_start = state.pool_time_remaining
+
+    -- schedule an infinite update loop at fixed frequency
+    timer_update_schedule =  Wait.time(update_timer, UPDATE_PERIOD, -1)
+end
+
+
+function instruct_start_earliest_timer()
+    -- instructs earliest relative (possibly self) to start
+    earliest_relative.call('start_timer')
+end
+
+
+function instruct_any_timer_running(value)
+    -- instructs relatives that one of the timers has started, which locks out UI editing
+    for relative, _ in pairs(family) do
+        relative.setVar('any_timer_running', value)
+    end
+end
+
+
+function reset_timer()
+    -- halts countdown loop and resets time fields
+
+    -- halt countdown via pause function
+    pause_timer()
+
+    -- reset time fields
+    state.pool_time_remaining = state.pool_time_original
+    pool_time_remaining_at_countdown_start = nil
+    state.bronstein_time_remaining = state.bronstein_time_original
+    state.counting_bronstein = true
+    self.editInput({index=pool_time_edit_index, value=format_time(state.pool_time_remaining, false)})
+    self.editInput({index=bronstein_time_edit_index, value=format_time(state.bronstein_time_remaining, true)})
+end
+
+
+function instruct_reset_timers()
+    -- instructs entire family to reset
+    for relative, _ in pairs(family) do
+        relative.call('reset_timer')
+        relative.setVar('any_timer_running', false)
+    end
+end
+
+
+function pause_timer(clear_global_running_state)
+    -- halts countdown loop
+    if state.timer_running then
+        -- halt loop
+        Wait.stop(timer_update_schedule)
+        timer_update_schedule = nil
+        countdown_start_time = nil
+        
+        -- set state
+        state.timer_running = false
+    end
+end
+
+
+function instruct_pause_timers()
+    -- instructs entire family to pause
+    for relative, _ in pairs(family) do
+        relative.call('pause_timer')
+        relative.setVar('any_timer_running', false)
+    end
 end
 
 
 function update_timer()
-    -- computes time difference since timer start
-    -- handles transition from bronstein timer to pool timer
-    -- handles timer run out
-    -- updates UI time elements
+    -- updates state and UI with timer status during a countdown
+    --     computes time difference since timer start
+    --     handles transition from bronstein timer to pool timer
+    --     handles timer run out
+    --     updates UI time elements
 
+    -- acquire time difference since start
+    time_difference = os.clock() - countdown_start_time
+
+    -- update time remaining state
+    if state.counting_bronstein then
+        -- acquire difference to bronstein timer
+        state.bronstein_time_remaining = state.bronstein_time_original - time_difference
+
+        -- check for bronstein timer run out
+        if state.bronstein_time_remaining <= 0 then
+            state.counting_bronstein = false
+            state.bronstein_time_remaining = 0  -- set to precisely 0 for a nice UI display
+        end
+
+        -- update UI
+        self.editInput({index=bronstein_time_edit_index, value=format_time(state.bronstein_time_remaining, true)})
+    end
+
+    if not state.counting_bronstein then  -- might enter this due to bronstein timer run out above
+        -- acquire difference to pool timer
+        pool_time_difference = time_difference - state.bronstein_time_original
+        state.pool_time_remaining = pool_time_remaining_at_countdown_start - pool_time_difference
+
+        -- check for pool time run out
+        if state.pool_time_remaining <= 0 then
+            state.pool_time_remaining = 0  -- set to precisely 0 for a nice UI display
+        end
+
+        -- update UI
+        self.editInput({index=pool_time_edit_index, value=format_time(state.pool_time_remaining, true)})
+
+        -- trigger next turn if pool time ran out
+        if state.pool_time_remaining <= 0 then
+            -- TODO play a sound effect
+            next_turn()
+        end
+    end
+end
+
+
+function next_turn()
+    -- Passes turn to the next player, pausing this timer, and starting the next timer
+
+    if not state.timer_running then
+        printToAll('This timer is not running, ignoring attempt to go to next turn.', 'White')
+    else
+        -- TODO play a sound effect on turn pass
+        pause_timer(false)
+        next_relative.call('start_timer')
+    end
 end
 
 
@@ -378,14 +527,14 @@ function onLoad(saved_state)
     -- 'state' will be stored and recovered across saves, other variables will not
     state = {
         turn = 1,
-        pool_time_original = 900,
-        pool_time_remaining = 900,
+        pool_time_original = 15*60,
+        pool_time_remaining = 15*60,
         bronstein_time_original = 15,
         bronstein_time_remaining = 15,
+        counting_bronstein = true,  -- if false, then counting pool time instead
         
         -- timer is always in one of 3 states: running, paused, or reset
         timer_running = false,
-        timer_paused = false,
         any_timer_running = false,  -- coordinates family activity
     }
     next_relative = nil  -- object cannot be serialized, and is recomputed on load anyway
@@ -393,9 +542,8 @@ function onLoad(saved_state)
     countdown_start_time = nil  -- absolute clock reference is not valid between saves
     pool_time_edit_index = nil  -- created on load
     bronstein_time_edit_index = nil  -- created on load
-
-    -- TODO TMP
-    test_button()
+    timer_update_schedule = nil  -- created on demand
+    pool_time_remaining_at_countdown_start = nil  -- created on demand. avoids differential time keeping for as long as possible
 
     -- recover personal state, if any
     if saved_state ~= nil and saved_state ~= '' then
@@ -409,9 +557,6 @@ function onLoad(saved_state)
         end
     end
 
-    -- TODO TMP
-    test_button()
-
     -- communicate with other timers
     add_self_to_family()
 
@@ -422,8 +567,5 @@ function onLoad(saved_state)
     if state.timer_running then
         start_timer()
     end
-
-    -- schedule update loop
-    -- TODO
 end
 
